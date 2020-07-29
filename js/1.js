8e1b4bf601269ced2f8e44b5205ce57589f248a8
Taboo-Yu <1343089310@qq.com>
2020-07-29 14:55:17 +0800

    ooo

---
+++ js/1.js
@@ -0,0 +1,40 @@
+/*
+ * @Author       : 俞涛
+ * @Date         : 2020-07-27 11:10:36
+ * @LastEditors  : 俞涛
+ * @LastEditTime : 2020-07-27 17:01:54
+ * @FilePath     : \CherryDiary-master\js\1.js
+ */
+
+/*pageone.html...*/
+$(document).on('pagecreate', '#mypage1', function () {
+  $('#mypage1').on('swipeleft', function () {
+    window.location.href = 'pagetwo.html'
+  })
+})
+$(document).on('pagecreate', '#mypage2', function () {
+  $('#mypage2').on('swipeleft', function () {
+    window.location.href = 'pagethree.html'
+  })
+})
+$(document).on('pagecreate', '#mypage2', function () {
+  $('#mypage2').on('swiperight', function () {
+    window.location.href = 'pageone.html'
+  })
+})
+$(document).on('pagecreate', '#mypage3', function () {
+  $('#mypage3').on('swipeleft', function () {
+    window.location.href = 'pagefour.html'
+  })
+})
+$(document).on('pagecreate', '#mypage3', function () {
+  $('#mypage3').on('swiperight', function () {
+    window.location.href = 'pagetwo.html'
+  })
+})
+
+$(document).on('pagecreate', '#mypage4', function () {
+  $('#mypage4').on('swiperight', function () {
+    window.location.href = 'pagethree.html'
+  })
+})
---
+++ js/covervid.js
@@ -0,0 +1,60 @@
+/*
+ * @Author       : 俞涛
+ * @Date         : 2020-07-27 11:10:36
+ * @LastEditors  : 俞涛
+ * @LastEditTime : 2020-07-27 17:02:06
+ * @FilePath     : \CherryDiary-master\js\covervid.js
+ */
+
+jQuery.fn.extend({
+  coverVid: function (width, height) {
+    // Call sizeVideo() on load and resize
+    $(document).ready(sizeVideo)
+    $(window).resize(sizeVideo)
+
+    // Define the attached selector
+    var $this = this
+
+    function sizeVideo() {
+      // Get parent element height and width
+      var parentHeight = $this.parent().height()
+      var parentWidth = $this.parent().width()
+
+      // Get native video width and height
+      var nativeWidth = width
+      var nativeHeight = height
+
+      // Get the scale factors
+      var heightScaleFactor = parentHeight / nativeHeight
+      var widthScaleFactor = parentWidth / nativeWidth
+
+      // Set necessary styles to position video "center center"
+      $this.css({
+        position: 'absolute',
+        top: '50%',
+        left: '30%',
+        '-webkit-transform': 'translate(-50%, -50%)',
+        '-moz-transform': 'translate(-50%, -50%)',
+        '-ms-transform': 'translate(-50%, -50%)',
+        '-o-transform': 'translate(-50%, -50%)',
+        transform: 'translate(-50%, -50%)'
+      })
+
+      // Set overflow hidden on parent element
+      $this.parent().css('overflow', 'hidden')
+
+      // Based on highest scale factor set width and height
+      if (widthScaleFactor > heightScaleFactor) {
+        $this.css({
+          height: 'auto',
+          width: parentWidth
+        })
+      } else {
+        $this.css({
+          height: parentHeight + 55,
+          width: 'auto'
+        })
+      }
+    }
+  }
+})
